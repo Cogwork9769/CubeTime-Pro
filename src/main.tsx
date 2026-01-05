33ca2d436aa2fd4b25399ceb33c95b6257892ea6
Brian Shelton <bshelton2031@csyonline.com>
2026-01-05 18:55:44 -0500

    Replace App component with TimerPage component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import TimerPage from "./pages/TimerPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <TimerPage />
   </React.StrictMode>
 );
